f411cf06d6c4aeb9a1878603d0c783eb9109124c
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-29 14:49:26 +0300

    layouts done

--- app/src/main/java/com/landfathich/githubsearchapp/MainActivity.kt
+++ app/src/main/java/com/landfathich/githubsearchapp/MainActivity.kt
@@ -2,10 +2,15 @@ package com.landfathich.githubsearchapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.landfathich.githubsearchapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
     }
 }
