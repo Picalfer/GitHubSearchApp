87e409ce30f6e07dd4b16faf3a2f80c73c20c49b
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-30 18:39:10 +0300

    sort lists

--- app/src/main/java/com/landfathich/githubsearchapp/ui/MainViewModel.kt
+++ app/src/main/java/com/landfathich/githubsearchapp/ui/MainViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.landfathich.githubsearchapp.data.api.RetrofitClient
 import com.landfathich.githubsearchapp.data.model.Item
+import com.landfathich.githubsearchapp.data.model.Repo
+import com.landfathich.githubsearchapp.data.model.User
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,10 +19,16 @@ class MainViewModel : ViewModel() {
         CoroutineScope(Dispatchers.IO).launch {
             val userResponse = RetrofitClient.apiInstance.getAllUsersByName(query)
             val userList = userResponse.items as ArrayList<Item>
+            with(userList as ArrayList<User>) {
+                sortBy { it.login.lowercase() }
+            }
             listItems.postValue(userList)
 
             val repoResponse = RetrofitClient.apiInstance.getAllReposByName(query)
             val repoList = repoResponse.items as ArrayList<Item>
+            with(repoList as ArrayList<Repo>) {
+                sortBy { it.name.lowercase() }
+            }
             listItems.postValue(repoList)
         }
     }
