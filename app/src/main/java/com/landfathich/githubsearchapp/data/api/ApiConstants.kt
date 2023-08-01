0fe53a40de111631914482a24515dc6cd303c408
Arthur Kozhemyakin <trable228@icloud.com>
2023-08-01 18:09:13 +0300

    token in apiConstants, data class

---
+++ app/src/main/java/com/landfathich/githubsearchapp/data/api/ApiConstants.kt
@@ -0,0 +1,5 @@
+package com.landfathich.githubsearchapp.data.api
+
+object ApiConstants {
+    const val token = "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"
+}
---
+++ app/src/main/java/com/landfathich/githubsearchapp/data/api/GitHubApi.kt
@@ -0,0 +1,34 @@
+package com.landfathich.githubsearchapp.data.api
+
+import com.landfathich.githubsearchapp.data.model.UserResponse
+import com.landfathich.githubsearchapp.data.model.RepoResponse
+import com.landfathich.githubsearchapp.data.model.RepoContent
+import com.landfathich.githubsearchapp.data.api.ApiConstants
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.Response
+import retrofit2.http.Headers
+import retrofit2.http.Query
+import retrofit2.http.Path
+
+interface GitHubApi {
+    @GET("search/users")
+    @Headers("Authorization: token $")
+    suspend fun getAllUsersByName(
+        @Query("q") query: String,
+    ): Response<UserResponse>
+
+    @GET("search/repositories")
+    @Headers("Authorization: token ${ApiConstants.token}")
+    suspend fun getAllReposByName(
+        @Query("q") query: String,
+    ): Response<RepoResponse>
+
+    @GET("/repos/{owner}/{repo}/contents/{path}")
+    @Headers("Authorization: token ${ApiConstants.token}")
+    suspend fun getRepoContent(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String,
+        @Path("path") path: String = ""
+    ): Response<RepoContent>
+}
